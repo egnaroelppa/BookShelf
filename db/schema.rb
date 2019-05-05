1a73247ab56658d0487fcb180b9ef415fe8bbaf5
EC2 Default User <ec2-user@ip-172-31-46-222.us-east-2.compute.internal>
2019-05-05 09:29:45 +0000

    add book_register_page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190505074719) do
+ActiveRecord::Schema.define(version: 20190505091957) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
